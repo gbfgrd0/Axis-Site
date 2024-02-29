f0bc72c5a1860c9b62330f48740f5e37da425515
Gabriel Figueiredo <118404050+gbfgrd0@users.noreply.github.com>
2024-02-28 22:22:57 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function openMenu(){
+    let menu = document.getElementById('menu');
+    menu.style.display = 'flex';
+}
+
+function closeMenu(){
+    let menu = document.getElementById('menu');
+    menu.style.display = 'none';
+}
